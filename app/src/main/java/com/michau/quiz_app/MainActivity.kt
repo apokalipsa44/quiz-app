12a235cf856cc7ac945d68f7447aa02e6d8ebbc7
apokalipsa44 <m_lachowicz@wp.pl>
2020-05-25 16:40:47 +0200

    bottom nav done

--- app/src/main/java/com/michau/quiz_app/MainActivity.kt
+++ app/src/main/java/com/michau/quiz_app/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.michau.quiz_app
 
+import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +15,80 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setViewPager()
     }
+
+    private fun setViewPager() {
+        viewpager.adapter = getFragmentPagerAdapter()
+        navigation.setOnNavigationItemSelectedListener(getBottomNavigationItemSelectedListener())
+        viewpager.addOnPageChangeListener(getOnPageChangeListener())
+        viewpager.offscreenPageLimit = 2
+    }
+
+    private fun getOnPageChangeListener() =
+        object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+//przy przewijaniu nic się nie dzieje - bo nie musi się dziać
+            }
+
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                //przy przerzucaniu całej strony na bok też nic się nie dzieje
+            }
+
+            override fun onPageSelected(position: Int) {
+                // przy wybieraniu strony ma się zmienic ikonka więc tu robimy
+                navigation.menu.getItem(position).isChecked = true
+            }
+        }
+
+
+    private fun getBottomNavigationItemSelectedListener() =
+        BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.navigation_home -> {
+                    viewpager.currentItem = 0
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_dashboard -> {
+                    viewpager.currentItem = 1
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_notifications -> {
+                    viewpager.currentItem = 2
+                    return@OnNavigationItemSelectedListener true
+                }
+                else -> {
+                    Log.wtf("Fragment out of bound", "What?")
+                    return@OnNavigationItemSelectedListener false
+                }
+            }
+        }
+
+    private fun getFragmentPagerAdapter() =
+        object : FragmentPagerAdapter(supportFragmentManager) {
+            override fun getItem(position: Int) = when (position) {
+                FEED_ID -> Fragment()
+                CHOOSER_ID -> Fragment()
+                PROFILE_ID -> Fragment()
+                else -> {
+                    Log.wtf("Fragment out of bound", "What?")
+                    Fragment()
+                }
+            }
+
+            override fun getCount(): Int {
+                return 3
+            }
+        }
+
+    companion object {
+        const val FEED_ID = 1
+        const val CHOOSER_ID = 2
+        const val PROFILE_ID = 3
+    }
+
 }
